f22091732c7c8cadec2c9b0756981b50ab829849
Jasraj Shendge <jasraj.229301558@muj.manipal.edu>
2025-01-27 23:51:44 +0530

    updated package.json

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -6,14 +6,20 @@ let  cached =(global as any).mongoose || {conn: null, promise: null};
 
 export const connectToDatabase = async () => {
     if (cached.conn) return cached.conn;
-    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-    
+    console.log('using existing database connection');
+    if(!MONGODB_URI){ 
+        console.error('MONGODB_URI is missing');
+        throw new Error('MONGODB_URI is missing');
+    }
+
+    console.log('new database connection');
     cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
         dbName :'evently',
         bufferCommands : false,
     })
 
     cached.conn = await cached.promise;
+    console.log('connected to database');
 
     return cached.conn;
 }
